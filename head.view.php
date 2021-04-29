e3cecc00bf012041aa8cf9a19330ff17588c307e
profegil <profe.gilberto.ramirez@gmail.com>
2021-04-29 12:14:42 -0500

    Add files via upload

---
+++ head.view.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <script src="https://kit.fontawesome.com/b0f70672d9.js" crossorigin="anonymous"></script>  
+        <link href="https://fonts.googleapis.com/css2?family=Piazzolla:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+        
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        
+        <title>Prolog-JSON</title>
+        <!--<script type='text/javascript'>
+            document.oncontextmenu = function(){return false};
+            document.onkeydown = function(){return false};
+        </script>-->
+
+        <script src="js/jquery-3.5.1.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/acordion.js"></script>
+    </head>
+
+    <body>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: #083b2a;">
+                <div class="container" style="padding:5px 5px">                    
+                    <a class="navbar-brand" href="#">
+                        <h5><i class="fas fa-file-code"></i> Ingeniería del conocimiento <i class="fas fa-code"></i></h5>
+                    </a>        
+
+                    <div class="collapse navbar-collapse">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item active"><a class="nav-link" href="index.php">Inicio</a></li>
+                            <li class="nav-item active"><a class="nav-link" href="clasificacion.php">Clasificación</a></li>
+                            <li class="nav-item active"><a class="nav-link" href="agrupacion.php">Agrupación</a></li>
+                            <li class="nav-item active"><a class="nav-link" href="impuestoAutos.php">Cálculo de ISAN</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
